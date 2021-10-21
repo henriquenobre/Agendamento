4d48a716f7817e9cf1a8ebb2f886892f0c4ea06b
nobre <henriquenobr@gmail.com>
2021-10-21 14:12:35 -0300

    next-images svg

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     presets: ["next/babel"],
-    plugins: [["styled-components", {"sstr": true }]]
+    plugins: [
+        ["styled-components", {"sstr": true }],
+        "inline-react-svg"
+    ]
 };

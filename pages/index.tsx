5f8a618798dc5df8fc014cf2e3068bd022a3ec8f
nobre <henriquenobr@gmail.com>
2021-10-21 11:34:46 -0300

    first boilerplate

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Agendamento</title>
+      </Head>
+
+      <main >
+        <h1 >
+         Helcome to Agendamento
+        </h1>
+
+      </main>
+    </div>
+  )
+}
